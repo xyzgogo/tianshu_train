9512497650531446d645cb65bb809880460c8251
hetangj <hetangj@isstech.com>
2023-07-28 08:01:08 +0000

    Update train.py

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ import argparse
 # --train_model_out=/workspace/model-out
 # train_out=/workspace/out
 # pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
-# pip install scikit-learn
+# pip install scikit-learn ultralytics
 
 def data_process():
     # 数据集切分
@@ -21,8 +21,8 @@ def data_process():
 def main():
     parser = argparse.ArgumentParser(description="Example script with long options.")
     # 添加长选项
-    parser.add_argument("--epochs", help="train epochs", default=10)
-    args = parser.parse_args()
+    parser.add_argument("--epochs", type=int, help="train epochs")
+    args, _ = parser.parse_known_args()
     # Load a model
     model = YOLO('/modeldir/yolov8n.pt')
     # Train the model
@@ -33,7 +33,13 @@ if __name__ == "__main__":
     data_process()
 
     source_path = "/workspace/out"
+    os.makedirs(source_path, exist_ok=True)
     target_path = "runs"  # 在当前目录下创建名为 "runs" 的软链接
     create_symlink(source_path, target_path)
 
     main()
+
+    source_directory = "/workspace/out/detect/train/weights"
+    destination_directory = "/workspace/model-out"
+
+    copy_directory(source_directory, destination_directory)
